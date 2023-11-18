3a9ce6a65a996731f7afbe1af79b50b0a22cb2e2
SabriCaballero <caballero.sabri@gmail.com>
2023-11-18 15:09:33 -0300

    Clase Especialidad

--- untitled/src/main/java/org/example/modelos/Especialidad.java
+++ untitled/src/main/java/org/example/modelos/Especialidad.java
@@ -5,4 +5,12 @@ import java.util.Date;
 
 @Entity
 public class Especialidad {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+
+    private String nombre;
+
+    @ManyToMany(mappedBy = "especialidades")
+    private List<Tecnico> tecnicos;
 }
