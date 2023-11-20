4dac0cfbb477b1e5b7a7ee85bb44d9b68b4c09b0
FrancoMCuello <franco.m.cuello@gmail.com>
2023-11-19 21:28:59 -0300

    correcciones en los modelos

--- untitled/src/main/java/org/example/Main.java
+++ untitled/src/main/java/org/example/Main.java
@@ -1,10 +1,21 @@
 package org.example;
 
+import org.hibernate.Session;
+import org.hibernate.Transaction;
+
 // Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
 // then press Enter. You can now see whitespace characters in your code.
 public class Main {
     public static void main(String[] args) {
+        try (Session session = org.integrador.hibernateConfig.HibernateConfig.getSessionFactory().openSession()) {
+            Transaction transaction = session.beginTransaction();
+
+            // Aca irian las acciones
+
+            transaction.commit();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
 
         }
     }
-}
--- untitled/src/main/java/org/example/modelos/Especialidad.java
+++ untitled/src/main/java/org/example/modelos/Especialidad.java
@@ -2,15 +2,46 @@ package org.example.modelos;
 
 import javax.persistence.*;
 import java.util.Date;
+import java.util.List;
 
 @Entity
 public class Especialidad {
-   // @Id
-   // @GeneratedValue(strategy = GenerationType.IDENTITY)
+    // @Id
+    // @GeneratedValue(strategy = GenerationType.IDENTITY)
     private long id;
 
     private String nombre;
-    
-   // @ManyToMany(mappedBy = "especialidades")
+
+    public Especialidad(long id, String nombre) {
+        this.id = id;
+        this.nombre = nombre;
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public List<Tecnico> getTecnicos() {
+        return tecnicos;
+    }
+
+    public void setTecnicos(List<Tecnico> tecnicos) {
+        this.tecnicos = tecnicos;
+    }
+
+    @ManyToMany(mappedBy = "especialidades")
+
     private List<Tecnico> tecnicos;
 }
